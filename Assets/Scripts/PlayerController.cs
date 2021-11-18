ce8b3c78a4514b835c32fe40a36beea8e86ac1a8
onandi-10 <gutierrezmon10@outlook.com>
2021-11-18 09:59:40 -0600

    Probando el mapa con un jugador 9:59am
    Se puso una capsula para asi probar el mapa y como se veria con el jugador.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    public CharacterController charCon;
+
+    private Vector3 moveInput;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        charCon.Move(moveInput);
+    }
+}
