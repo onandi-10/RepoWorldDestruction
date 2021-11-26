5e343142574c89efe5efb556fdd92305e2a9f943
solrac98 <carlosssxp@gmail.com>
2021-11-26 09:39:25 -0600

    test

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+
+    public float moveSpeed, lifeTime;
+
+    public Rigidbody theRB;
+
+    public GameObject impactEffect;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = transform.forward * moveSpeed;
+
+        lifeTime -= Time.deltaTime;
+
+        if(lifeTime <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+        }
+
+        Destroy(gameObject);
+        Instantiate(impactEffect, transform.position, transform.rotation);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
+    public float jumpPower;
+    private bool canJump;
+    public Transform groundCheckPoint;
+    public LayerMask whatIsGround;
 
     public CharacterController charCon;
 
@@ -12,10 +16,16 @@ public class PlayerController : MonoBehaviour
 
     public Transform camTrans;
 
+    public float mouseSensitivity;
+
     public static PlayerController instance;
 
+    public GameObject bullet;
+    public Transform firePoint;
 
-    void Awake(){
+
+    void Awake()
+    {
         instance = this; 
     }
 
@@ -29,15 +39,31 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        //moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        //moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
-        charCon.Move(moveInput);
+        Vector3 vertMove = transform.forward * Input.GetAxis("Vertical");
+        Vector3 horiMove = transform.right * Input.GetAxis("Horizontal");
 
+        moveInput = horiMove + vertMove;
+        moveInput.Normalize();
+        moveInput = moveInput * moveSpeed;
+
+        canJump = Physics.OverlapSphere(groundCheckPoint.position, .25f, whatIsGround).Length > 0;
+
+        //Salto
+        if (Input.GetButtonDown("Jump") && canJump)
+        {
+            moveInput.y = jumpPower;
+        }
+
+        charCon.Move(moveInput * Time.deltaTime);
+
+        //Control Rotacion Camara
 
         Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
 
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
-        camTrans.rotation = Quaternion.Euler(camTrans.rotation.eulerAngles + new Vector3(-mouseInput.y, 0f, 0f));
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, -mouseInput.x, 0f));
+        //camTrans.rotation = Quaternion.Euler(camTrans.rotation.eulerAngles + new Vector3(-mouseInput.y, 0f, 0f));
     }
 }
